076af0b981d833110044f4d4f825a7e6a6716c2a
Luis Mendez <dluismendezdj@gmail.com>
2023-05-31 22:52:41 -0400

    refactor: optimizar metodos de mapping y transformacion de datos

--- src/utils/dataTransform.js
+++ src/utils/dataTransform.js
@@ -3,228 +3,129 @@
  * El objeto de datos transformado contiene las propiedades "columns" y "rows".
  *
  * @param {Object} data - El objeto de datos original con las propiedades "columns" y "rows".
+ * @param {Object} mappings - El objeto de mapeo que especifica las propiedades de destino y las rutas para obtener los valores correspondientes del objeto de datos original.
  * @returns {Object} - El objeto de datos transformado con las filas filtradas y mapeadas.
  */
-const transformInvoiceData = (data) => {
+const transformData = (data, mappings) => {
     const { rows, columns } = data;
 
-    const transformedRows = rows
-        // eslint-disable-next-line camelcase
-        .reduce((accumulator, { id, fecha_contabilizacion, estatus, montoAplicado, caja, vendedor, cliente }) => {
-            const { descripcion: cajaDescripcion } = caja;
-            const { nombre: clienteNombre, descripcion: clienteDescripcion } = cliente;
-            const { nombres: vendedorNombre, apellidos: apellidoVendedor } = vendedor;
+    const transformedRows = rows.reduce((accumulator, row) => {
+        const newRow = Object.entries(mappings).reduce((obj, [key, value]) => {
+            const properties = value.split('.');
+            let nestedValue = row[properties[0]];
 
-            const newRow = {
-                id,
-                // eslint-disable-next-line camelcase
-                fecha_contabilizacion,
-                vendedor,
-                estatus,
-                importe: montoAplicado,
-                'caja.descripcion': cajaDescripcion,
-                'vendedor.nombre': `${vendedorNombre} ${apellidoVendedor}`,
-                'cliente.nombre': clienteNombre,
-                descripcion: clienteDescripcion
-            };
+            // eslint-disable-next-line no-plusplus
+            for (let i = 1; i < properties.length; i++) {
+                if (nestedValue) {
+                    nestedValue = nestedValue[properties[i]];
+                } else {
+                    break;
+                }
+            }
 
-            accumulator.push(newRow);
-            return accumulator;
-        }, []);
+            obj[key] = nestedValue;
+            return obj;
+        }, {});
+
+        accumulator.push(newRow);
+        return accumulator;
+    }, []);
 
     return {
         columns,
         rows: transformedRows
     };
+};
+
+const transformInvoiceData = (data) => {
+    const mappings = {
+        id: 'id',
+        fecha_contabilizacion: 'fecha_contabilizacion',
+        vendedor: 'vendedor.nombres',
+        estatus: 'estatus',
+        importe: 'montoAplicado',
+        'caja.descripcion': 'caja.descripcion',
+        'vendedor.nombre': 'vendedor.nombres',
+        'cliente.nombre': 'cliente.nombre',
+        descripcion: 'cliente.descripcion'
+    };
+
+    return transformData(data, mappings);
 };
 
 const transformUserData = (data) => {
-    const { rows, columns } = data;
-
-    const transformedRows = rows
-        // eslint-disable-next-line camelcase
-        .reduce((accumulator, { id, descripcion, apellidos, email, rol, telefono, estatus }) => {
-            const { descripcion: userRol } = rol;
-
-            const newRow = {
-                id,
-                descripcion,
-                apellidos,
-                email,
-                'rol.descripcion': userRol,
-                telefono,
-                estatus
-            };
-
-            accumulator.push(newRow);
-            return accumulator;
-        }, []);
-
-    return {
-        columns,
-        rows: transformedRows
+    const mappings = {
+        id: 'id',
+        descripcion: 'descripcion',
+        apellidos: 'apellidos',
+        email: 'email',
+        'rol.descripcion': 'rol.descripcion',
+        telefono: 'telefono',
+        estatus: 'estatus'
     };
+
+    return transformData(data, mappings);
 };
 
 const transformInventoryData = (data) => {
-    const { rows, columns } = data;
-
-    const transformedRows = rows
-        // eslint-disable-next-line camelcase
-        .reduce((accumulator, { id, sucursal, descripcion, estatus }) => {
-            const { descripcion: sucursalDescription } = sucursal;
-
-            const newRow = {
-                id,
-                'sucursal.descripcion': sucursalDescription,
-                descripcion,
-                estatus
-            };
-
-            accumulator.push(newRow);
-            return accumulator;
-        }, []);
-
-    return {
-        columns,
-        rows: transformedRows
+    const mappings = {
+        id: 'id',
+        'sucursal.descripcion': 'sucursal.descripcion',
+        descripcion: 'descripcion',
+        estatus: 'estatus'
     };
+
+    return transformData(data, mappings);
 };
 
 const transformProductData = (data) => {
-    const { rows, columns } = data;
-
-    const transformedRows = rows
-        // eslint-disable-next-line camelcase
-        .reduce(
-            (
-                accumulator,
-                {
-                    id,
-                    descripcion,
-                    sucursal,
-                    almacen,
-                    // eslint-disable-next-line camelcase
-                    tipo_producto,
-                    // eslint-disable-next-line camelcase
-                    clase_producto,
-                    // eslint-disable-next-line camelcase
-                    cantidad_stock,
-                    // eslint-disable-next-line camelcase
-                    cantidad_comprometida,
-                    // eslint-disable-next-line camelcase
-                    cantidad_disponible,
-                    estatus
-                }
-            ) => {
-                const { descripcion: sucursalDescription } = sucursal;
-                const { descripcion: almacenDescription } = almacen;
-                // eslint-disable-next-line camelcase
-                const { descripcion: tipoProductoDescription } = tipo_producto;
-                // eslint-disable-next-line camelcase
-                const { descripcion: claseProductoDescription } = clase_producto;
-
-                const newRow = {
-                    id,
-                    descripcion,
-                    'sucursal.descripcion': sucursalDescription,
-                    'almacen.descripcion': almacenDescription,
-                    'tipo_producto.descripcion': almacenDescription,
-                    'clase_producto.descripcion': almacenDescription,
-                    // eslint-disable-next-line camelcase
-                    cantidad_stock,
-                    // eslint-disable-next-line camelcase
-                    cantidad_comprometida,
-                    // eslint-disable-next-line camelcase
-                    cantidad_disponible,
-                    estatus
-                };
-
-                accumulator.push(newRow);
-                return accumulator;
-            },
-            []
-        );
-
-    return {
-        columns,
-        rows: transformedRows
+    const mappings = {
+        id: 'id',
+        descripcion: 'descripcion',
+        'sucursal.descripcion': 'sucursal.descripcion',
+        'almacen.descripcion': 'almacen.descripcion',
+        'tipo_producto.descripcion': 'tipo_producto.descripcion',
+        'clase_producto.descripcion': 'clase_producto.descripcion',
+        cantidad_stock: 'cantidad_stock',
+        cantidad_comprometida: 'cantidad_comprometida',
+        cantidad_disponible: 'cantidad_disponible',
+        estatus: 'estatus'
     };
+
+    return transformData(data, mappings);
 };
 
 const transformPaymentMethodData = (data) => {
-    const { rows, columns } = data;
-
-    const transformedRows = rows
-        // eslint-disable-next-line camelcase
-        .reduce((accumulator, { id, descripcion, sucursal, estatus }) => {
-            const { nombre: sucursalNombre } = sucursal;
-
-            const newRow = {
-                id,
-                descripcion,
-                'sucursal.descripcion': sucursalNombre,
-                estatus
-            };
-
-            accumulator.push(newRow);
-            return accumulator;
-        }, []);
-
-    return {
-        columns,
-        rows: transformedRows
+    const mappings = {
+        id: 'id',
+        descripcion: 'descripcion',
+        'sucursal.descripcion': 'sucursal.nombre',
+        estatus: 'estatus'
     };
+
+    return transformData(data, mappings);
 };
 
 const transformCreditCardData = (data) => {
-    const { rows, columns } = data;
-
-    const transformedRows = rows
-        // eslint-disable-next-line camelcase
-        .reduce((accumulator, { id, descripcion, sucursal, estatus }) => {
-            const { descripcion: sucursaldescripcion } = sucursal;
-
-            const newRow = {
-                id,
-                descripcion,
-                'sucursal.descripcion': sucursaldescripcion,
-                estatus
-            };
-
-            accumulator.push(newRow);
-            return accumulator;
-        }, []);
-
-    return {
-        columns,
-        rows: transformedRows
+    const mappings = {
+        id: 'id',
+        descripcion: 'descripcion',
+        'sucursal.descripcion': 'sucursal.descripcion',
+        estatus: 'estatus'
     };
+
+    return transformData(data, mappings);
 };
 
 const transformTurnsData = (data) => {
-    const { rows, columns } = data;
-
-    const transformedRows = rows
-        // eslint-disable-next-line camelcase
-        .reduce((accumulator, { id, descripcion, sucursal, estatus }) => {
-            const { descripcion: sucursaldescripcion } = sucursal;
-
-            const newRow = {
-                id,
-                descripcion,
-                'sucursal.descripcion': sucursaldescripcion,
-                estatus
-            };
-
-            accumulator.push(newRow);
-            return accumulator;
-        }, []);
-
-    return {
-        columns,
-        rows: transformedRows
+    const mappings = {
+        id: 'id',
+        descripcion: 'descripcion',
+        'sucursal.descripcion': 'sucursal.descripcion',
+        estatus: 'estatus'
     };
+
+    return transformData(data, mappings);
 };
 
 export {
